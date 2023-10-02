fddd2b80aa3672b606914a7779c2d1063cd564fd
christian magallanes <dar4del-x@hotmail.com>
2023-10-02 03:11:00 -0300

    hola

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+function servicios() {
+  let seccion = document.getElementById("servicios");
+  seccion.scrollIntoView({ behavior: "smooth" });
+}
+function about() {
+  let seccion = document.getElementById("sobremi");
+  seccion.scrollIntoView({ behavior: "smooth" });
+}
+const verPlanesBtn = document.getElementById("ver-planes-btn");
+
+verPlanesBtn.addEventListener("click", function(){
+  window.location.href = './pages/planes.html';
+});
